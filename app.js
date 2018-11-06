daa35c571b028762d5d13fa52b8682a7a389936c
Ian Richard <ianrichard03@gmail.com>
2018-11-05 23:24:33 -0600

    changed root.

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const app = express();
 //uses a call back that takes in request and sends a response.
 //usses http request from node.
 
-app.get('/Haiku/Fall', function(request, response) {
+app.get('/', function(request, response) {
     response.send('Fall walking westward,\nlong drunken shadows looming,\nthe tired sun sets.');
 });
 
-app.get('/Haiku/Duck', function(request, response) {
+app.get('/Duck', function(request, response) {
     response.send('Webbed fisher fowl,\nslipstreaming the lillypads,\nmuck is your supper.');
 });
 
-app.get('/Haiku/Samurai', function(request, response) {
+app.get('/Samurai', function(request, response) {
     response.send('Cherryblossom tree,\nhappily I bleed in shade,\nmy last stroke was strong.');
 });
 //express has a listen method that allows you to assign
